ecdf7adca308fca35caa0a5579817dc06c38c4ce
rolaxx <xjejevbx@mail.com>
2016-02-08 00:35:53 +0100

    Création du menu de navigation et insertion du logo

---
+++ sources/PHP/header.php
@@ -0,0 +1,26 @@
+<nav class="navbar navbar-inverse col-xs-12" style="height:115px;" role="navigation">
+    <div class="row">
+        <div class="col-xs-8">
+            <div class="navbar-header row">
+                <img style="width:125px;" src="/ProjetDeployezUnSiteEnLigne/sources/images/logo.png" id="logopng" alt="Logo ayze" />
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <div class="collapse navbar-collapse row">
+                <ul class="nav navbar-nav">
+                    <li> <a href="#contact"><span>Acceuil</span></a> </li>
+                    <li> <a href="#accueil"><span>Actualités</span></a> </li>
+                    <li> <a href="#contact"><span>Plus d'infos</span></a> </li>
+                    <li> <a href="#qui"><span>Activités du mois</span></a> </li>
+                    <li> <a href="#contact"><span>Contact</span></a> </li>
+                </ul>
+            </div>
+        </div>
+        <div class="col-xs-4">
+            <?php include 'membre.php'; ?>
+        </div>
+    </div>
+</nav>
---
+++ sources/PHP/index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <title>Ayze</title>
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/ProjetDeployezUnSiteEnLigne/sources/CSS/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <div class="container-fluid">
+        <header class="row">
+            <?php include 'header.php'; ?>
+        </header>
+
+
+
+        <footer>
+            <?php include 'footer.php'; ?>
+        </footer>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    <script>
+        $(function () {
+            $(window).scroll(function () {
+                if ($(this).scrollTop() == 0) {
+                    
+                    
+                    
+                } else {
+                    
+                    
+                    
+                }
+            });
+        });
+    </script>
+</body>
+
+</html>
