b471b1f94c878d721e0f97753378fabfa134b9f1
rolaxx <xjejevbx@mail.com>
2016-02-09 04:06:03 +0100

    Résolution du problème de formulaire.

--- sources/includes/creationActiNews.php
+++ sources/includes/creationActiNews.php
@@ -5,16 +5,16 @@
             <h2>Creation de news</h2>
             <label for="titre">*Titre :</label>
             <br>
-            <input type="text" placeholder="Titre de la news" id="titre"/>
+            <input name="titre" type="text" placeholder="Titre de la news"/>
         </div>
         <div>
             <label for="texte">*Texte de la news :</label>
             <br>
-            <textarea name="text" placeholder="Texte de la news" id="texte"></textarea>
+            <textarea name="texte" placeholder="Texte de la news"></textarea>
         </div>
         <div>
             <label for="image">Lien d\'une image :</label>
-            <input type="text" placeholder="http://static.wamiz.fr/images/animaux/chiens/large/chien-courant-de-posavatz.jpg" id="image"/>
+            <input name="image" type="text" placeholder="http://static.wamiz.fr/images/animaux/chiens/large/chien-courant-de-posavatz.jpg"/>
             <br>
             <br>
         </div>
@@ -25,22 +25,22 @@
 
 <!-- Corps du cadre création d'activités -->
 <span id="cadreCreationActivite" class="whiteText">
-    <form class="cadreVert" action="../PHP/creationActivite.php" methode="post">
+    <form class="cadreVert" action="../PHP/creationActivite.php" method="post">
         <div>
             <h2>Creation d'activités</h2>
             <label for="titre">*Titre :</label>
             <br>
-            <input type="text" placeholder="Titre de l'activité" id="titre"/>
+            <input type="text" placeholder="Titre de l'activité" name="titre" />
         </div>
         <div>
             <label for="texte">*Texte de l'activité :</label>
             <br>
-            <textarea name="text" placeholder="Texte de l'activité" id="texte"></textarea>
+            <textarea placeholder="Texte de l'activité" name="texte" ></textarea>
         </div>
         <div>
             <label for="date">*Date de l'évenement :</label>
             <br>
-            <input type="text" placeholder="18 février 2016" id="date"/><br><br>
+            <input type="text" placeholder="18 février 2016" name="date" /><br><br>
         </div>
         <p>* Obligatoire &nbsp;</p>
         <input class="blackText" type="submit" name="Valider" />
