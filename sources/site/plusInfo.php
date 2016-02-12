8c4e8b288babb5ec98b2f57543665495fc262c2f
rolaxx <xjejevbx@mail.com>
2016-02-12 02:35:20 +0100

    modification du liens des images

--- sources/site/plusInfo.php
+++ sources/site/plusInfo.php
@@ -16,34 +16,34 @@
             </header>
             <div id="presentationVille" class="row">
                 <p class="col-xs-12 whiteText">La ville dispose de plusieurs activités, voilà une liste des principales activités culturel disponible :</p><br><br>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Marathon :</h2>
-                    <img class="imgNews" src="http://www.u-run.fr/wp-content/uploads/2014/06/senior_marathon.jpg"/>
+                    <img class="imgNews" src="../images/marathon.jpg"/>
                     <p>Le marathon est une course organisée pour récolter des fonds pour l'asosiation piece jaune. Principalement fait en été.</p>
                 </div>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Musée de la ville :</h2>
-                    <img class="imgNews" src="http://www.ecole-sainte-odile.fr/assets/upload/news_img/424/file-5621d0398d8b1.jpg"/>
+                    <img class="imgNews" src="../images/musee.jpg"/>
                     <p>Le musée est ouvert du lundi au samedi de 8H à 19H</p>
                 </div>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Tir à l'arc :</h2>
-                    <img class="imgNews" src="http://www.valjoly.com/wp-content/uploads/2013/07/tirAlArc-slide2.jpg"/>
+                    <img class="imgNews" src="../images/tirAlArc-slide2.jpg"/>
                     <p>Notre ville dispose d'un stande de tir à l'arc ouvert à tous. La mairie peut éventuellement prêter le materièle (<i>Flèches et Arc</i>). Pour ce faire, veuillez nous contacter vià l'onglet Contact</p>
                 </div>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Cyber-café :</h2>
-                    <img class="imgNews" src="http://www.undernews.fr/wp-content/uploads/2010/11/cybercafes.jpg"/>
+                    <img class="imgNews" src="../images/cybercafes.jpg"/>
                     <p>Ayze propose un Cyber-café placer au centre de la ville. Vous pouvez accèder aux ordinateurs gratuitement si vous faites partie de la commune.</p>
                 </div>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Tombola :</h2>
-                    <img class="imgNews" src="http://www.onlinetri.com/sites/triath-lons/graphics/images/1438426098.jpg"/>
+                    <img class="imgNews" src="../images/tombola.jpg"/>
                     <p>Toutes les fin de mois, une tombola est organisée pour récolter des dons pour la croix rouge.</p>
                 </div>
-                <div class="whiteText col-md-4 cadreVertNews">
+                <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Food-Truck :</h2>
-                    <img class="imgNews" src="http://assets.productionswow.com.s3.amazonaws.com/wp-content/uploads/2013/05/Avatar-camion.png"/>
+                    <img class="imgNews" src="../images/Avatar-camion.png"/>
                     <p>Ayze possède plusieurs emplacement de Food-Trucks disponnible, pour vous rensseigner sur la disponibiliter d'un emplacement, veuillez nous contacter vià l'onglet Contacts.</p>
                 </div>
             </div>
@@ -51,7 +51,7 @@
                 <?php include '../includes/footer.php'; ?>
             </footer>
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/5.5.5/js/bootstrap.min.js"></script>
             <script src="../JS/indexJS.js"></script>
     </body>
 
