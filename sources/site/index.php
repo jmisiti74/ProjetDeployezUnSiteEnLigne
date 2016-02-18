7759683509c47163a8d774d8e3c1f25f3330bd05
rolaxx <xjejevbx@mail.com>
2016-02-18 13:13:04 +0100

    Correction des fautes

--- sources/site/index.php
+++ sources/site/index.php
@@ -14,27 +14,27 @@
             <header class="row">
                 <?php include '../includes/header.php'; ?>
             </header>
-			
-			<?php include '../includes/formulaires.php'; ?>
-			
-			
-            <div id="presentationVille">
-                <div class="row">
-                    <p class="col-md-4 whiteText">Bienvenue sur le site de la ville d'ayze (<i>ayse</i>), vous trouverez sur ce site différentes informations sur la ville mais aussis un éspace d'inscription pour différents type d'activités culturel et sportive proposé par la commune. Vous pouvez nous contacter directement vià le formulaire de contact mis a disposition en cliquand sur le lien "Contact" dans le menu de navigation.
-                    </p>
-                </div>
-                <div class="row">
-                    <div class="col-md-4 whiteText">
-                        <?php include '../PHP/latestNews.php' ?>
+
+            <?php include '../includes/formulaires.php'; ?>
+
+
+                <div id="presentationVille">
+                    <div class="row">
+                        <p class="col-md-4 whiteText">Bienvenue sur le site de la ville d'Ayze (<i>Ayse</i>), vous trouverez sur ce site différentes informations sur la ville mais aussi un espace d'inscription pour différents types d'activitées culturelles et sportives proposées par la commune. Vous pouvez nous contacter directement via le formulaire de contact mis à disposition en cliquant sur le lien "Contact" dans le menu de navigation.
+                        </p>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-4 whiteText">
+                            <?php include '../PHP/latestNews.php' ?>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <footer class="row navbar navbar-fixed-bottom">
-                <?php include '../includes/footer.php'; ?>
-            </footer>
-            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
-            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-            <script src="../JS/indexJS.js"></script>
+                <footer class="row navbar navbar-fixed-bottom">
+                    <?php include '../includes/footer.php'; ?>
+                </footer>
+                <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
+                <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+                <script src="../JS/indexJS.js"></script>
     </body>
 
     </html>
--- sources/site/plusInfo.php
+++ sources/site/plusInfo.php
@@ -18,36 +18,36 @@
 			<?php include '../includes/formulaires.php'; ?>
 			
             <div id="presentationVille" class="row">
-                <p class="col-xs-12 whiteText">La ville dispose de plusieurs activités, voilà une liste des principales activités culturel disponible :</p><br><br>
+                <p class="col-xs-12 whiteText">La ville dispose de plusieurs activitées, voilà une liste des principales activitées culturelles disponibles :</p><br><br>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Marathon :</h2>
                     <img class="imgNews" src="../images/marathon.jpg"/>
-                    <p>Le marathon est une course organisée pour récolter des fonds pour l'asosiation piece jaune. Principalement fait en été.</p>
+                    <p>Le marathon est une course organisée pour récolter des fonds pour l'association Pièces Jaunes. Principalement fait en Été.</p>
                 </div>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Musée de la ville :</h2>
                     <img class="imgNews" src="../images/musee.jpg"/>
-                    <p>Le musée est ouvert du lundi au samedi de 8H à 19H</p>
+                    <p>Le musée est ouvert du lundi au samedi de 8H à 19H.</p>
                 </div>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Tir à l'arc :</h2>
                     <img class="imgNews" src="../images/tirAlArc-slide2.jpg"/>
-                    <p>Notre ville dispose d'un stande de tir à l'arc ouvert à tous. La mairie peut éventuellement prêter le materièle (<i>Flèches et Arc</i>). Pour ce faire, veuillez nous contacter vià l'onglet Contact</p>
+                    <p>Notre ville dispose d'un stande de tir à l'arc ouvert à tous. La mairie peut éventuellement prêter le materiel (<i>Flèches et Arcs</i>). Pour ce faire, veuillez nous contacter via l'onglet Contact.</p>
                 </div>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Cyber-café :</h2>
                     <img class="imgNews" src="../images/cybercafes.jpg"/>
-                    <p>Ayze propose un Cyber-café placer au centre de la ville. Vous pouvez accèder aux ordinateurs gratuitement si vous faites partie de la commune.</p>
+                    <p>Ayze propose un Cyber-café placé au centre de la ville. Vous pouvez accèder aux ordinateurs gratuitement si vous faites partie de la commune.</p>
                 </div>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Tombola :</h2>
                     <img class="imgNews" src="../images/tombola.jpg"/>
-                    <p>Toutes les fin de mois, une tombola est organisée pour récolter des dons pour la croix rouge.</p>
+                    <p>Toutes les fin de mois, une tombola est organisée pour récolter des dons pour la Croix-Rouge.</p>
                 </div>
                 <div class="whiteText col-md-5 cadreVertNews">
                     <h2>Food-Truck :</h2>
                     <img class="imgNews" src="../images/Avatar-camion.png"/>
-                    <p>Ayze possède plusieurs emplacement de Food-Trucks disponnible, pour vous rensseigner sur la disponibiliter d'un emplacement, veuillez nous contacter vià l'onglet Contacts.</p>
+                    <p>Ayze possède plusieurs emplacement de Food-Trucks, pour vous renseigner sur la disponibiliter d'un emplacement veuillez nous contacter via l'onglet Contacts.</p>
                 </div>
             </div>
             <footer class="row navbar navbar-fixed-bottom">
